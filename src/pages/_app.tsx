8be3cf55f2ccfb60972ae04593c332d6dfe8400a
Antony.Sex <a.sessa0915@gmail.com>
2023-02-27 00:02:46 +0100

    Add GTM script by using Next.js guide

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,27 @@
+import Script from "next/script";
 import React, { useEffect } from "react";
 import { GlobalStyle } from "../components/GlobalStyle";
-import TagManager from "react-gtm-module"
-
-const tagManagerArgs = {
-  id: "GTM-5RR53JT",
-}
 
 export default function App({ Component, pageProps }) {
-
-  useEffect(() => {
-    TagManager.initialize(tagManagerArgs)
-  }, [])
-  
   return (
     <>
       <GlobalStyle />
       <Component {...pageProps} />
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GTM-5RR53JT"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GTM-5RR53JT');
+        `}
+      </Script>
+      
     </>
   );
 }
