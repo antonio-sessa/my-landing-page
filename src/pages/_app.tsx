45ed887619b9f3919fa9daf8b457d4ea3bc4d3ea
Antony.Sex <a.sessa0915@gmail.com>
2023-02-26 23:44:12 +0100

    Add GTM to my website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GlobalStyle } from "../components/GlobalStyle";
+import Script from 'next/script'
+import TagManager from "react-gtm-module"
+
+const tagManagerArgs = {
+  id: "GTM-5RR53JT",
+}
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+  
   return (
     <>
       <GlobalStyle />
