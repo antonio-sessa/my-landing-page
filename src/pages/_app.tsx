bc114d34a4bb7daf3d7d9136344b92ae67485029
Antonio Sessa <a.sessa11@gmail.com>
2023-02-27 00:07:33 +0100

    Add GTM to my website (#1)
    * Add GTM to my website
    
    * Adjust tag manager args
    
    * Add GTM script by using Next.js guide

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import Script from "next/script";
+import React, { useEffect } from "react";
 import { GlobalStyle } from "../components/GlobalStyle";
 
 export default function App({ Component, pageProps }) {
@@ -6,6 +7,21 @@ export default function App({ Component, pageProps }) {
     <>
       <GlobalStyle />
       <Component {...pageProps} />
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GTM-5RR53JT"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GTM-5RR53JT');
+        `}
+      </Script>
+      
     </>
   );
 }
