93e1df7d0fc36d3feb953a4910b5032b14185a1c
antonio-sessa <antonio.sessa@prontopro.it>
2023-02-28 12:37:19 +0100

    Add Cookiebot banner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,19 +9,39 @@ export default function App({ Component, pageProps }) {
       <Component {...pageProps} />
 
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=GTM-5RR53JT"
+        data-cookieconsent="ignore"
+        id="google-analytics"
         strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
+      >
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
+          gtag("consent", "default", {
+            ad_storage: "denied",
+            analytics_storage: "granted",
+            wait_for_update: 500
+          });
+          gtag("set", "ads_data_redaction", true);
+          gtag("set", "url_passthrough", true);
           gtag('js', new Date());
 
           gtag('config', 'GTM-5RR53JT');
         `}
       </Script>
-      
+
+      <Script
+        data-cookieconsent="ignore"
+        src="https://www.googletagmanager.com/gtag/js?id=GTM-5RR53JT"
+        strategy="afterInteractive"
+      />
+
+      <Script
+        id="Cookiebot"
+        src="https://consent.cookiebot.com/uc.js"
+        data-cbid="b98e1d1b-1839-4473-b290-41f8d4c11788"
+        data-blockingmode="auto"
+        type="text/javascript"
+      ></Script>
     </>
   );
 }
