6927b8e83941ee463fc82dbed33fb09abf048d71
antonio-sessa <antonio.sessa@prontopro.it>
2023-05-30 15:12:59 +0200

    Modify the logic behind lastmod field in sitemap, and update metadata about website

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -5,15 +5,16 @@ export const Meta = () => {
   return (
     <>
       <title>
-        Antonio Sessa - QA Specialist, Photographer, Curious about life
+        Antonio Sessa - Passionate Quality Assurance Professional and
+        Photographer in Milan, IT
       </title>
       <meta
         name="description"
-        content="Antonio Sessa.
-    Dealing with Quality Assurance, which I discovered over time to be my passion, and I made a job of it.
-    Working in dynamic teams, being able to express myself, giving (and above all receiving!) suggestions: these are the values I like to leverage.
-    Currently based in Milan, IT."
+        content="Antonio Sessa. Discover how my passion for Quality Assurance drives dynamic teams, fosters creativity, and delivers exceptional results.
+        Experience the captivating reality I bring to life through my photography!
+        Currently based in Milan, IT."
       />
+
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/media/images/favicon.png" />
       <link rel="canonical" href="https://antoniosessa.com" />
