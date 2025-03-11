22a998dc938c7fe8e4065c0642504967a732546e
antonio-sessa <antonio.sessa@prontopro.it>
2025-03-11 10:06:49 +0100

    Update sitemap dates, improve TypeScript configuration reference, and upgrade dependencies

--- src/scripts/generate-sitemap.js
+++ src/scripts/generate-sitemap.js
@@ -1,29 +1,28 @@
 const fs = require("fs");
-const globby = require("globby");
-
-const buildTime = new Date();
-
-function addPage(page) {
-  const path = page
-    .replace("src/pages", "")
-    .replace(".tsx", "")
-    .replace("index", "");
-  const route = path === "/" ? "" : path;
-
-  return `
-  <url>
-    <loc>${`${process.env.WEBSITE_URL}${route}`}</loc>
-    <lastmod>${`${buildTime.toISOString()}`}</lastmod>
-  </url>`;
-}
 
 async function generateSitemap() {
-  // Ignore Next.js specific files (e.g., _app.js) and API routes.
+  const { globby } = await import("globby"); // Dynamic import
   const pages = await globby([
     "src/pages/**/*{.tsx,.mdx}",
     "!src/pages/_*.tsx",
     "!src/pages/api",
   ]);
+
+  const buildTime = new Date();
+  function addPage(page) {
+    const path = page
+      .replace("src/pages", "")
+      .replace(".tsx", "")
+      .replace("index", "");
+    const route = path === "/" ? "" : path;
+
+    return `
+    <url>
+      <loc>${`${process.env.WEBSITE_URL}${route}`}</loc>
+      <lastmod>${`${buildTime.toISOString()}`}</lastmod>
+    </url>`;
+  }
+
   const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${pages.map(addPage).join("\n")}
   </urlset>`;
